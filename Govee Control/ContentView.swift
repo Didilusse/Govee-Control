21d1d51b78c62e75d962a7e24c6dd81266cb1084
Adil Rahmani <didilusse@gmail.com>
2025-05-17 21:03:44 -0400

    Initial Commit

---
+++ Govee Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Govee Control
+//
+//  Created by Adil Rahmani on 5/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Govee Control/Govee_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Govee_ControlApp.swift
+//  Govee Control
+//
+//  Created by Adil Rahmani on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Govee_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
